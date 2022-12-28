13b242511ce65c8623402416405328779481c12e
Berfongel <121492984+Berfongel@users.noreply.github.com>
2022-12-28 15:52:14 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+int derece;
+Console.WriteLine("Suyun sıcaklık derecesini giriniz ");
+derece = Convert.ToInt32(Console.ReadLine());
+if (derece < 0)
+{
+    Console.WriteLine("0 derecenin altında olduğundan katı halde");
+}
+else if (0 < derece || derece < 100)
+{
+    Console.WriteLine("0 - 100 derece arasında olduğundan sıvı halde");
+}
+else 
+{
+    Console.WriteLine("100 dereceden fazla olduğundan gaz halde");
+}
+
